c2dc0d04d1db1247787cbe6a3c44cd6578c7b01c
bryan005-d <brayane.chavarrou@ecci.edu.co>
2024-09-23 19:15:38 -0500

    valores

---
+++ mapping.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+contract Mapping {
+    // Definimos un mapping de direcciones y enteros
+    mapping(address => uint256) balances; 
+
+    // funcion para establecer el balance de Avax en el mapping
+    function setBalance(address _addr) public  {
+        uint256 avaxBalance = _addr.balance;
+        balances[_addr] = avaxBalance;
+    }
+
+    // Funcion para obtener el valor del mapping en la direccion indice
+    function getBalance(address _addr) public view returns (uint256) {
+        return balances[_addr];
+    }
+
+    // Funcion me permite eliminar un valor del mapping
+    function remove(address _addr) public {
+        delete balances[_addr];
+    }
+
+    // Funcion que llma otra funcion ya creada para actualizar los balances del mapping
+    function updateBalanceAvax(address _addr) public {
+        setBalance(_addr);
+    }
+
+}
